68f907d8987fc4d89a0850bdb5054331881d3639
Amy Dach <adach@live.com>
2019-08-13 14:56:08 -0500

    Python Homework 0810 Updates 0813

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -33,6 +33,7 @@ with open(csvpath, 'r') as file_handler:
 # In[5]:
 
 
+candidates_votes=[]
 with open(csvpath, 'r') as csvfile:
     
     # CSV reader specifies delimiter and variable that holds contents
@@ -43,23 +44,86 @@ with open(csvpath, 'r') as csvfile:
     header = next(csvreader)
     #print(f" CSV Header: {csv_header}")
     Voter_index=header.index("Voter ID")
+    Candidate_index=header.index("Candidate")
     for row in csvreader:
-        Voter=row[Voter_index]
-        row_count=sum(1 for row in csvreader)+1
-        print("```text")
-        print("Election Results")
-        print("----------------------------")
-        print("Total Votes:" + " " + str(Voter))
+        #Voter=row[Voter_index]
+        candidates_votes.append(row[Candidate_index])
+        #row_count=sum(1 for row in csvreader)+1
+        #print("```text")
+        #print("Election Results")
+        #print("----------------------------")
+        #print("Total Votes:" + " " + str(row_count))
 
 
 # In[6]:
 
 
-with open(csvpath,"r") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    header=next(csvreader)
-    Candidate_index=header.index("Candidate")
-    for row in csvreader:
-        Candidate=row[Candidate_index]
-        #print(Candidate)
+candidate_list=list(set(candidates_votes))
+candidate_list
+
+
+# In[7]:
+
+
+#Percentage of votes each candidate won
+total_votes=len(candidates_votes)
+total_votes
+
+
+# In[8]:
+
+
+votes_per_candidate=[]
+for candidate in candidate_list:
+    votes_per_candidate.append(candidates_votes.count(candidate))
+    votes_per_candidate
+
+percentages=[round(100*vote/total_votes,4) for vote in votes_per_candidate]
+percentages
+
+
+# In[9]:
+
+
+#print("```text")
+#print("Election Results")
+#print("----------------------------")
+#print("Total Votes:" + " " + str(total_votes))
+
+
+# In[10]:
+
+
+for i in range(0,len(candidate_list)):
+    print(f"The candidate {candidate_list[i]} has {votes_per_candidate[i]} votes and {percentages[i]} %")
+
+
+# In[11]:
+
+
+max_votes = max(votes_per_candidate)
+
+for row in range(0,len(candidate_list)):
+    Total = max_votes
+    f=open("txt","w+")
+    with open('txt','w') as out:
+        line1 = ("```text")
+        line2 = ("Election Results")
+        line3 = ("----------------------------")
+        line4 = ("Total Votes: " + " " + str(total_votes))
+        line5 = ("----------------------------")
+        line6 = ("The candidate " + str(candidate_list[0]) + " has " + str(votes_per_candidate[0]) + " votes " + str(percentages[0])+ "%")
+        line7 = ("The candidate " + str(candidate_list[1]) + " has " + str(votes_per_candidate[1]) + " votes " + str(percentages[1])+ "%")
+        line8 = ("The candidate " + str(candidate_list[2]) + " has " + str(votes_per_candidate[2]) + " votes " + str(percentages[2])+ "%")
+        line9 = ("The candidate " + str(candidate_list[3]) + " has " + str(votes_per_candidate[3]) + " votes " + str(percentages[3])+ "%")
+        line10 = ("----------------------------")
+        line11 = ("Winner:" + " " + str(Total))
+        line12 = ("----------------------------")
+        line13 = ("````")
+        out.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13))
+f.close()
+# I know that candidates s/b ranked in descending order, but can't figure it out.
+# I know the winner s/b Khan, but can't get the corresponding name to appear as winner.
+# I can't figure out how to get the candidate list to output to the txt, so I give up.
+# So, I have hard coded the row number of each candidate in the list in lines 6-9, which is not the best but provides the results.
 
--- Pybank/main.py
+++ Pybank/main.py
@@ -56,9 +56,9 @@ with open(csvpath,"r") as csvfile:
        monthly_changes.append(monthly_change)
        previous_profit=current_profit
    monthly_changes = monthly_changes[1:]
-   #average=sum(monthly_changes)/(total_months - 1)
-   increase=max(monthly_changes)
-   decrease=min(monthly_changes)
+   average=round(sum(monthly_changes)/(len(total_months) - 1),2)
+   increase=round(max(monthly_changes),0)
+   decrease=round(min(monthly_changes),0)
    dateincrease=monthly_changes.index(increase)
    datedecrease=monthly_changes.index(decrease)
    
@@ -66,7 +66,32 @@ with open(csvpath,"r") as csvfile:
    print("Financial Analysis")
    print("----------------------------")
    print("Total Months:" + " " + str(Total_Month))
-   print("Average Change: ")# + "$" + str(average))
-   print("Greatest Increase: " + str(total_months[dateincrease + 1]) + " $" + str(increase))
-   print("Greatest Decrease: " + str(total_months[datedecrease + 1]) + " $" + str(decrease)) 
+   print("Total:" + "$" + str(round(Total,0)))
+   print("Average Change: " + "$" + str(average))
+   print("Greatest Increase: " + str(total_months[dateincrease + 1]) + " ($" + str(increase)+ ")")
+   print("Greatest Decrease: " + str(total_months[datedecrease + 1]) + " ($" + str(decrease)+ ")") 
+
+
+# In[6]:
+
+
+Total_Month = 0
+csvpath = os.path.join("Resources","budget_data.csv")
+f=open("txt","w+")
+with open(csvpath,"r") as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    header=next(csvreader)
+    for row in csvreader:
+        Total_Month += 1
+        with open('txt','w') as out:
+            line1 = ("```text")
+            line2 = ("Financial Analysis")
+            line3 = ("----------------------------")
+            line4 = ("Total Months: " + str(Total_Month))
+            line5 = ("Total:" + "$" + str(round(Total,0)))
+            line6 = ("Average Change: " + "$" + str(average))
+            line7 = ("Greatest Increase: " + str(total_months[dateincrease + 1]) + " ($" + str(increase)+ ")")
+            line8 = ("Greatest Decrease: " + str(total_months[datedecrease + 1]) + " ($" + str(decrease)+ ")") 
+            out.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7,line8))
+f.close()
 
